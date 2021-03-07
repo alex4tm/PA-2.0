4a786ed892e5007e1702dc1ebec7d6cad2be61fb
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-03-07 19:13:47 +0400

    edit/update and destroy methods implemented

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-
+  before_action :set_category, only: [:destroy, :edit, :update]
   def index
     @categories = Category.all
   end
@@ -18,7 +18,26 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @category.update(category_params)
+
+    redirect_to categories_path(@category)
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path
+  end
+
 private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
   def category_params
     params.require(:category).permit(:name)
   end
