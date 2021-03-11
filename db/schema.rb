998c123e7ee81548531304ca81d695e3b0f184c5
sadiq alli <allisadiq36@gmail.com>
2021-03-11 15:23:29 +0400

    inputs for device

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_03_10_075906) do
+ActiveRecord::Schema.define(version: 2021_03_11_103255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +97,7 @@ ActiveRecord::Schema.define(version: 2021_03_10_075906) do
     t.string "username"
     t.bigint "restaurant_id"
     t.string "secure_id"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["restaurant_id"], name: "index_users_on_restaurant_id"
