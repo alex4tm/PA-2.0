70044756f97fde83abfdeeaefe27be51b6e396eb
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-05-28 16:43:58 +0400

    added user and restaurant demo buttons for auto login on landing page , replaced placehlder avatar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @user = User.first
+    @restaurant = User.find(98)
   end
 end
