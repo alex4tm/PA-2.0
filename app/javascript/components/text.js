025cf4d1a9c8b5689c84c8d3b57615b8a9aa2a40
Alexandru Iliesi <iliesialexandru@gmail.com>
2021-05-29 21:21:03 +0400

    adjusted animated text duration and delay

--- app/javascript/components/text.js
+++ app/javascript/components/text.js
@@ -11,14 +11,14 @@ const text = () => {
         targets: '.ml6 .letter',
         translateY: ["1.1em", 0],
         translateZ: 0,
-        duration: 750,
+        duration: 1500,
         delay: (el, i) => 50 * i
       }).add({
         targets: '.ml6',
         opacity: 0,
-        duration: 1000,
+        duration: 2000,
         easing: "easeOutExpo",
-        delay: 1000
+        delay: 3000
       });
   }
 }
